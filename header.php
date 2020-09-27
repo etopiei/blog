bc96c5962a4e62c783d459bab1641d4e0ab5799b
etopiei <lj343@icloud.com>
2020-09-27 17:35:15 +1000

    Make header also add a title to page

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <head>
     <link href="<?php echo $base; ?>/main.css" rel="Stylesheet" type="text/css">
+    <title><?php echo $title; ?></title>
     <script src="https://use.fontawesome.com/9afece952d.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/rainbow.min.css" integrity="sha256-hXLM0/qy7ngStQk25tspI7LR0OJOdqeaDbe5AJDUGgM=" crossorigin="anonymous" />
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.0/highlight.min.js"></script>
